7e78bd864f076b13835dbb54655fa4baabf0ee02
David <ddmace@gmail.com>
2019-09-04 00:04:07 +0200

    Initial import

---
+++ src/main/java/org/dmace/hbn/simpleblog/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package org.dmace.hbn.simpleblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome(Model model) {
+        model.addAttribute("message", "welcome to spring booy");
+        return "index";
+    }
+}
